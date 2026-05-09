93e490c7816feb0013476b861049b6bf3f5a7267
arinone <enzohochin@gmail.com>
2026-05-09 18:57:26 +0200

    Updated the project version & __all__ section.

--- SCPSIG/__init__.py
+++ SCPSIG/__init__.py
@@ -1,13 +1,13 @@
 __title__       = "Simple Crappy Pseudo-Shader Image Generation"
-__description__ = "A pretty pointless image generation library "
-__version__     = "0.1.0"
+__description__ = "A pretty pointless image generation library made for very specific needs."
+__version__     = "0.2.0"
 __author__      = "Arinone"
 __email__       = "no"
 __license__     = "MIT"
 __url__         = "https://github.com/you/SCPSIG"
 __status__      = "Development"
 
-__all__ = ["color", "Color", "uv", "UV", "generate_image"]
+__all__ = ["Color", "colors", "UV", "points", "generate_image"]
 
 from .generation import generate_image
 from .color import Color
